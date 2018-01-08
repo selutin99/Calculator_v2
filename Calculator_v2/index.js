19bd88b5d8808e43e49347e86526cc5704267b51
Alexander <cool.selutin99@yandex.ru>
2018-01-08 17:21:03 +0400

    Main

--- Calculator_v2/index.js
+++ Calculator_v2/index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import App from './src/App'
 AppRegistry.registerComponent('Calculator_v2', () => App);
