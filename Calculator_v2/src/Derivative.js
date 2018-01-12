ed42d3855ec6d27a31f0920d4378c58b1a73161a
Alexander <cool.selutin99@yandex.ru>
2018-01-12 20:10:05 +0400

    DerivativeActivity

--- Calculator_v2/src/Derivative.js
+++ Calculator_v2/src/Derivative.js
@@ -1,43 +1,42 @@
 const React = require('react');
 const {
+	AppRegistry,
     View,
     Text,
+	Button,
+	TextInput,
     StyleSheet
 } = require('react-native');
 const { Component } = React;
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
+
+onPressDoNothing=()=>{
+}
 
 export default class AboutPage extends Component {
     constructor(props){
         super(props)
+		this.state = {text: ''};
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Это тестовая страница!
-                </Text>
-                <Text style={styles.instructions}>
-                    Здесь будут производные!
-                </Text>
-            </View>
+            <View style={{padding: 10}}>
+				<TextInput
+				  style={{height: 40}}
+				  placeholder="Введите формулу"
+				  onChangeText={(text) => this.setState({text})}
+				/>
+				<Button
+				  onPress={onPressDoNothing}
+				  title="Найти производную"
+				  color="#f5901d"
+				/>
+				<Text style={{padding: 10, fontSize: 11}}>
+					Результаты:
+				</Text>
+				<Text style={{padding: 10, fontSize: 23}}>
+					
+				</Text>
+			</View>
         );
     }
 };
